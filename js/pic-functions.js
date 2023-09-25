9adeab220caa5ce5a125c4c4fb9de934d9ca6bae
Elizabeth Kodjo <ellizakodjo@outlook.com>
2023-09-25 09:01:37 +0100

    Functions created for various buttons

--- js/pic-functions.js
+++ js/pic-functions.js
@@ -1,8 +1,24 @@
+
+
+$(function () {
+    $('#email-form').submit(function(event) {
+        event.preventDefault();
+    });
+});
+
 // Getting the entered email address
 
 $(function () {
     $('#sub-btn').click(function () {
-        console.log("This is the email entered: " + $('#input-email').val());
+        console.log("This is the email entered: " + $('#email').val());
+    });
+});
+
+// Showing the next picture
+$(function () {
+    $('#next-btn').click(function () {
+        // console.log('This should change the pic');
+        $('.imgChoice').attr("https://picsum.photos/300?random=2");
     });
 });
 
@@ -10,13 +26,16 @@ $(function () {
 // Fetch and display email entered
 $(function () {
     $('#sub-btn').click(function () {
-        $('.recEmail').text($('#input-email').val());        
+        $('.recEmail').text($('#email').val());        
     });
 });
 
 // Email validation
-
-
-function validateEmail() {
-    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(String(email).toLowerCase());
-}
+// blank email
+$(function () {
+    $('#sub-btn').click(function () {
+        if ($(!'#email')) {
+            $('.errorMsg').text('Please enter an email address');
+        }
+    });    
+});
--- js/validation.js
+++ js/validation.js
@@ -1,5 +1,31 @@
-let email = document.querySelector('#input-email');
+const form = document.getElementById('email-form');
+const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+const email = document.getElementById('email');
+const errorMsg = document.querySelector('.errorMsg');
+const emailValue = email.value.trim();
 
-if (!email) {
+// if (form) {
+//     form.addEventListener('submit', e => {
+//         e.preventDefault();
+//     });
+// }
 
+// function isValidEmail() {
+//     return emailRegex.test(emailValue);
+// }
+
+// if (!emailValue) {
+//     console.log('Please enter an email');
+// }
+
+// if (!isValidEmail(emailValue)) {
+//     console.log('please enter a valid email');
+// }
+
+// Validating email
+
+function emailValidation() {
+    if (!email) {
+        console.log('Please enter an email address');
+    }
 }
