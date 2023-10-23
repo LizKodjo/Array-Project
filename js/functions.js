ca8c2a7008787f49845bca1c07f8beb603dbb5bb
Elizabeth Kodjo <ellizakodjo@outlook.com>
2023-10-23 20:40:21 +0100

    Validation and styling

--- js/functions.js
+++ js/functions.js
@@ -1,35 +1,56 @@
 const nextImg = document.querySelector('.nextbtn');
 const imgBox = document.querySelector('.random-img');
 const emailForm = document.querySelector('#form');
-let email = document.querySelector('#email');
+const email = document.querySelector('#email');
 const errorMsg = document.querySelector('.errorMsg');
 const subBtn = document.querySelector('.submitbtn');
-const getImages = document.querySelector('showImages');
+const displayImages = document.querySelector('.showImages');
+// const emailReg = '/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/';
+
+// Email array
+var imgEmail = [];
 
 
 // Prevent submit button from reloading page
-emailForm.addEventListener('submit', (e) => {
-    let messages = [];
+emailForm.addEventListener('submit', (e) => {    
+        e.preventDefault();    
+});
 
-    if (!email.value) {
-        messages.push('Please enter your email address.');
-    }
 
-    if (messages.length > 0) {
-        e.preventDefault();
-        errorMsg.innerText = messages.join(', ');
-    }
-    
-})
 
 // Get next image
 
 nextImg.addEventListener('click', () => {
-   imgBox.src = "https://picsum.photos/300?random=1";
+    imgBox.src = "https://picsum.photos/300?random=1";
 })
 
-// Get email and display
+// Validate email and display
 
-$('.submitbtn').click(function(){
-    $('.email').append($('.showImages').text());
-});
+function CheckEmail() {
+    let messages = [];
+
+    if (!email.value) {
+        messages.push('Please enter your email address.');
+    } else if  (!email.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+        messages.push('Please enter a valid address');
+    } else {
+        // imgEmail.push(email.value);
+        // console.log(imgEmail)
+        displayImages.innerHTML = email.value;
+        emailForm.reset();
+    }    
+    errorMsg.innerText = messages.join(', ');
+    
+}
+
+subBtn.addEventListener('click', CheckEmail);
+
+// Check email duplication
+
+function emailDuplication() {
+    if (imgEmail.value === email.value) {
+        errorMsg.innerHTML = 'Please enter a different email address.';
+    }
+}
+
+subBtn.addEventListener('click', emailDuplication);
